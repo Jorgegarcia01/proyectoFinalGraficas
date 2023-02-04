3d13eee96ad156c00360c641fa4ee4a233374e3c
jorgegarciaaa <43385254+Jorgegarcia01@users.noreply.github.com>
2023-02-03 21:10:50 -0600

    Colors and movement script

--- BuildABrick_ProyFinalGraficas/Assets/movement.cs
+++ BuildABrick_ProyFinalGraficas/Assets/movement.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class movement : MonoBehaviour
 {
+    public float velocidad;
+    private Rigidbody rigid;
+    public float velRotacion;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigid = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigid.AddForce(transform.forward * velocidad);
+        }
+        gameObject.transform.Rotate(0, Input.GetAxis("Horizontal") * velRotacion, 0);
     }
 }
